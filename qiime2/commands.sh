bdda426a30444ee80d1269530682a5d5a4c53ef9
Farhad Sadat <farhadsadat.ms@gmail.com>
2025-10-27 17:40:20 +0100

    Update commands.sh

--- qiime2/commands.sh
+++ qiime2/commands.sh
@@ -1,8 +1,16 @@
+(base) farhads-MacBook-Pro:thesis-benchmark farhadsadat$ conda activate qiime2-env
+(qiime2-env) farhads-MacBook-Pro:thesis-benchmark farhadsadat$ ROOT="$HOME/thesis-benchmark" 
+(qiime2-env) farhads-MacBook-Pro:thesis-benchmark farhadsadat$ mkdir -p "$ROOT/qiime"
 
-# 1) Import (already demultiplexed — 1 fastq per sample)
-qiime tools import \
-  --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path ../meta/manifest.tsv \
-  --output-path demux.qza \
-  --input-format PairedEndFastqManifestPhred33V2
+(qiime2-env) farhads-MacBook-Pro:thesis-benchmark farhadsadat$ qiime tools import \
+>   --type 'SampleData[PairedEndSequencesWithQuality]' \
+>   --input-path "$ROOT/manifest.tsv" \
+>   --output-path "$ROOT/qiime/demux-pe.qza" \
+>   --input-format PairedEndFastqManifestPhred33V2
+Imported /Users/farhadsadat/thesis-benchmark/manifest.tsv as PairedEndFastqManifestPhred33V2 to /Users/farhadsadat/thesis-benchmark/qiime/demux-pe.qza
+
+(qiime2-env) farhads-MacBook-Pro:thesis-benchmark farhadsadat$ qiime demux summarize \
+>   --i-data "$HOME/thesis-benchmark/qiime/demux-pe.qza" \
+>   --o-visualization "$HOME/thesis-benchmark/qiime/demux-pe.qzv"
+Saved Visualization to: /Users/farhadsadat/thesis-benchmark/qiime/demux-pe.qzv
 
